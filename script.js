c91d6d9be69ba326385b1c26f6b320512dabe57e
EliteDima <82768524+EliteDima@users.noreply.github.com>
2024-09-24 09:04:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript can be used to add interactivity or handle form submissions, animations, etc.
+// Here is a basic alert example
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to DataTech - Your trusted partner in IT solutions");
+});
